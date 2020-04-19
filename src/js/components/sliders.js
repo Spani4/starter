52fca5019f134ba16d40c40638ed0bbebf028f83
Anton <youz.sani4@gmail.com>
2020-04-19 14:27:45 +0300

    added gitkeep, sliders component, edited resets styles

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -2,6 +2,13 @@ import "swiper/css/swiper.min.css";
 import Swiper from 'swiper';
 import sliderConfig from '../configs/sliders';
 
+// export function initMainSlider() {
+//     const section = document.querySelector('.main-slider');
+//     if ( section == null ) return;
+//     const swiperContainer = section.querySelector('.swiper-container');
+//     const swiper = new Swiper(swiperContainer, sliderConfig.main);
+// }
+
 export default function() {
-    
+    // initMainSlider();
 }
